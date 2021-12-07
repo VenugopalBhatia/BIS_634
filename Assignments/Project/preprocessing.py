7a0a1f3b75adfdd9fd426fb4d2f1bae1085fc6ca
Venugopal Bhatia <venugopalbhatia@Venugopals-MacBook-Pro.local>
2021-12-06 19:55:52 -0500

    Project

---
+++ Assignments/Project/preprocessing.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import nltk
+from nltk.stem import WordNetLemmatizer
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+import re
+
+class Preprocessor:
+
+    def __init__(self):
+        self.lemmatizer = WordNetLemmatizer()
+        self.stopwords = set(stopwords.words('english'))
+        self.stopwords.update(list(string.punctuation))
+        self.pos_tag = nltk.pos_tag
+
+######### Methods to apply to dataframes ###############################################
+    
+    def simple_clean(self,df,msgColumn,cleanColumn):
+        df['retweeted_from'] = df[msgColumn].apply(lambda x: re.findall("^RT.*?:\s",x)) #Extract retweet handle
+        df['mentions'] = df[msgColumn].apply(lambda x: re.findall("@([a-zA-Z0-9_]{1,50})",x)) #Extract mentions
+        df['hashtags'] = df[msgColumn].apply(lambda x: re.findall("#([a-zA-Z0-9_]{1,50})",x)) #Extract hashtags
+        df['links'] = df[msgColumn].apply(lambda x: re.findall("https[a-zA-Z0-9_//:.]{1,100}",x)) #Extract links
+        df[cleanColumn] = df[msgColumn].replace("^RT.*?:|\s{2,}|(…)|(&amp;)|(’)|(”)|(“)|@([a-zA-Z0-9_]{1,50})|[^A-Za-z0-9_]{1}|https[a-zA-Z0-9_//:.]{1,100}"," ",regex=True).str.strip() #Dont remove hashtags as they may provide context
+    
+    def clean_tweets(self,df,msgColumn,wordTokens):
+        df[wordTokens] = df[msgColumn].apply(self.gen_word_tokens) # not converting to lowercase since it may lead to loss of information
+        
+    
+    def lemmatize_tweets(self,df,wordTokens,wordTokensLemmatized):
+        df[wordTokensLemmatized] = df[wordTokens].apply(self.lemmatize_message)
+
+######### Methods to apply at the row level ######################################
+
+    def gen_word_tokens(self,message):
+        words = word_tokenize(message)
+        wordList = []
+        for w in words:
+            if w.lower() not in self.stopwords:
+                wordList.append(w)
+        return wordList
+        
+    def lemmatize_message(self,wordTokens):
+        posTags = self.pos_tag(wordTokens)
+        lemmatized_words = []
+        for w in posTags:
+            lemmatized_words.append(self.lemmatizer.lemmatize(w[0],pos=self.get_simple_tag(w[1])).lower())
+        
+        return lemmatized_words
+    
+    def get_simple_tag(self,tag):
+        if tag.startswith('J'):
+            return wordnet.ADJ
+        elif tag.startswith('V'):
+            return wordnet.VERB
+        elif tag.startswith('N'):
+            return wordnet.NOUN
+        elif tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return wordnet.NOUN
+
+    
+
+
+    
+
+
+    
+
---
+++ Assignments/Project/utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+from nltk.probability import FreqDist
+from sklearn.feature_extraction.text import CountVectorizer
+from wordcloud import WordCloud,STOPWORDS
+from PIL import Image
+
+#### Method to create word cloud ####
+
+def create_wordcloud(text,targetFileName):
+    stopwords = set(STOPWORDS)
+    wc = WordCloud(background_color="white",
+    max_words=10000,
+    stopwords=stopwords,
+    repeat=True)
+    wc.generate(str(text))
+    img_file_path = "./data/" + targetFileName + ".png"
+    wc.to_file(img_file_path)
+    print("Word Cloud Saved Successfully")
+    display(Image.open(img_file_path))
+
+
+#### Method to extract n features by frequency ####
+
+def get_features(df,n,message_tokenized,features = 'features',lemmatized_message = 'lemmatized_message',lib = 'sklearn'):
+    if(lib == 'nltk'):
+        all_words = []
+        df[message_tokenized].apply(lambda x: add_words(all_words,x))
+        freq = FreqDist(all_words)
+        common = freq.most_common(n)
+        feature_set = [i[0] for i in common]
+        df[features] = df[message_tokenized].apply(lambda x: get_word_dict(feature_set,x))
+    else:
+        if(lemmatized_message not in df.columns):
+            df[lemmatized_message] = df[message_tokenized].apply(lambda x: " ".join(x))
+        count_vec = CountVectorizer(max_features=n)
+        data_matrix = count_vec.fit_transform(df[lemmatized_message].values)
+        data_matrix = data_matrix.todense()
+        feature_names = count_vec.get_feature_names()
+        return data_matrix,feature_names,count_vec
+
+def get_word_dict(feature_set,x):
+    words_set = set(x)
+    features = {}
+
+    for j in feature_set:
+        features[j] = j in words_set
+
+    return features
+
+ 
+def add_words(master_list,word_list):
+    master_list.extend(word_list)
+
+
+
